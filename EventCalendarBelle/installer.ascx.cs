d6e900677950e248ce2305bb6b75b781339ce03c
David Brendel <mantus667@googlemail.com>
2015-11-08 21:26:22 +0100

    Install Script
    Updated version number in install script

--- EventCalendarBelle/installer.ascx.cs
+++ EventCalendarBelle/installer.ascx.cs
@@ -21,7 +21,7 @@ namespace EventCalendarBelle
     public partial class installer : Umbraco.Web.UmbracoUserControl
     {
         private UmbracoDatabase _db = null;
-        protected Version newVersion = new Version("2.2.0");
+        protected Version newVersion = new Version("2.3.0");
         private Version oldVersion = new Version("2.0.0");
 
         protected void Page_Load(object sender, EventArgs e)

0c3c14aa8a0296ac545f37398c001f2e9ad27f7d
David Brendel <mantus667@googlemail.com>
2015-11-08 21:16:04 +0100

    Revent Resource
    Updated revent resource

--- EventCalendarBelle/EventCalendar/revent.resource.js
+++ EventCalendarBelle/EventCalendar/revent.resource.js
@@ -41,12 +41,12 @@
                     return $http.get(Umbraco.Sys.ServerVariables.eventCalendar.descriptionBaseUrl + "GetRTEConfiguration");
                 },
 
-                getPaged: function (type, itemsPerPage, pageNumber, sortColumn, sortOrder, searchTerm) {
+                getPaged: function (calendar, itemsPerPage, pageNumber, sortColumn, sortOrder, searchTerm) {
                     if (sortColumn == undefined)
                         sortColumn = "";
                     if (sortOrder == undefined)
                         sortOrder = "";
-                    return $http.get(Umbraco.Sys.ServerVariables.eventCalendar.reventBaseUrl + "GetPaged?itemsPerPage=" + itemsPerPage + "&pageNumber=" + pageNumber + "&sortColumn=" + sortColumn + "&sortOrder=" + sortOrder + "&searchTerm=" + searchTerm);
+                    return $http.get(Umbraco.Sys.ServerVariables.eventCalendar.reventBaseUrl + "GetPaged?calendar=" + calendar + "&itemsPerPage=" + itemsPerPage + "&pageNumber=" + pageNumber + "&sortColumn=" + sortColumn + "&sortOrder=" + sortOrder + "&searchTerm=" + searchTerm);
                 }
             };
         });
